ca03655d72703d3e5f555ff30f05b35044360cbe
Nithish <130085124+NithishCodes-2411@users.noreply.github.com>
2025-01-10 18:56:25 -0500

    Time: 3 ms (27.93%), Space: 12.8 MB (5.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,14 +1,23 @@
 class Solution(object):
     def uniquePaths(self, row, col):
-        
-        dp = [1 for _ in range(col)]
-        for r in range(1,row):
-            left = 0
-            for c in range(col):
-                dp[c] = left + dp[c]
-                left = dp[c]
-        return dp[col-1]
+        def f(r,c):
+            if r==0 and c==0:
+                return 1
+            if r <0 or c<0 :
+                return 0
+            if (r,c) not in dp:
+                dp[(r,c)] = f(r-1,c) + f(r,c-1)
+            return dp[(r,c)]
 
+        # dp = [1 for _ in range(col)]
+        # for r in range(1,row):
+        #     left = 0
+        #     for c in range(col):
+        #         dp[c] = left + dp[c]
+        #         left = dp[c]
+        # return dp[col-1]
+        dp = dict()
+        return f(row-1,col-1)
 
         
 
