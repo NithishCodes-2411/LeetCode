32de659f049139e4ec8ab3f95a8c77ee8bc6f65c
Nithish <130085124+NithishCodes-2411@users.noreply.github.com>
2025-01-09 20:03:09 -0500

    Time: 0 ms (100%), Space: 12.5 MB (18.97%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,20 +1,34 @@
 class Solution(object):
     def rob(self, nums):
-        dp = dict()
-        def help(i):
-            if i == 0:
-                return nums[i]
-            if i<0:
-                return 0
-            if i in dp:
-                return dp[i]
-            pick = nums[i]+ help(i-2)
-            not_pick = help(i-1)
-            dp[i] = max(pick,not_pick)
-            return dp[i]
+        # dp = dict()
+        # def help(i):
+        #     if i == 0:
+        #         return nums[i]
+        #     if i<0:
+        #         return 0
+        #     if i in dp:
+        #         return dp[i]
+        #     pick = nums[i]+ help(i-2)
+        #     not_pick = help(i-1)
+        #     dp[i] = max(pick,not_pick)
+        #     return dp[i]
             
-        help(len(nums)-1)
-        return nums[0] if len(nums)-1 == 0 else dp[len(nums)-1]
+        # help(len(nums)-1)
+        # return nums[0] if len(nums)-1 == 0 else dp[len(nums)-1]
+        n = len(nums)
+        dp = [-1] * n 
+
+        prev2 = 0
+        prev = nums[0]
+
+        for i in range(1,n):
+            pick = nums[i] + prev2
+            not_pick = prev
+            curr = max(pick,not_pick)
+            prev2 = prev
+            prev = curr
+        
+        return prev
 
         
         
