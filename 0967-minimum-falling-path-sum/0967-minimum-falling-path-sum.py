b0b059f8c584e132113982e0617dea64e905b47f
Nithish <130085124+NithishCodes-2411@users.noreply.github.com>
2025-01-11 19:41:05 -0500

    Time: 21 ms (81.51%), Space: 13 MB (55.46%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
@@ -1,21 +1,61 @@
 class Solution(object):
     def minFallingPathSum(self, matrix):
+
         row , col = len(matrix) , len(matrix[0])
 
-        def f(r,c):
-            if  c < 0 or c >= col:
-                return float("inf")
-            if r == row-1:
-                return matrix[r][c]
-            if (r,c) in dp:
-                return dp[(r,c)]
-            dp[(r,c)] =  matrix[r][c] + min(f(r+1,c+1) ,f(r+1,c-1) ,f(r+1,c))
-            return dp[(r,c)]
-            
-        ans = float("inf")
-        for j in range(col):
-            dp = dict()
-            ans = min(ans,f(0,j))
-        return ans 
+        dp = matrix[0]
+
+        for r in range(1,row):
+            new_dp = []
+            for c in range(col):
+                v = matrix[r][c]
+                m = dp[c]
+                if c > 0:
+                    m = min(m,dp[c-1])
+                if c < col-1:
+                    m = min(m,dp[c+1])
+                
+                new_dp.append(v+m)
+            dp = new_dp
+        
+        return min(dp)
+
+
+
+        # dp = [ [float("inf") for _ in range(col)] for _ in range(row)]
+
+        # for c in range(col):
+        #     dp[0][c] = matrix[0][c]
+        
+        # for r in range(1,row):
+        #     for c in range(col):
+        #         dp[r][c] = matrix[r][c]
+        #         m = dp[r-1][c]
+        #         if c > 0:
+        #             m = min(m,dp[r-1][c-1])
+        #         if c < col-1:
+        #             m = min(m,dp[r-1][c+1])
+                
+        #         dp[r][c] += m
+        
+        # return min(dp[row-1])
+
+
+        # def f(r,c):
+        #     if  c < 0 or c >= col:
+        #         return float("inf")
+        #     if r == row-1:
+        #         return matrix[r][c]
+        #     if (r,c) in dp:
+        #         return dp[(r,c)]
+        #     dp[(r,c)] =  matrix[r][c] + min(f(r+1,c+1) ,f(r+1,c-1) ,f(r+1,c))
+        #     return dp[(r,c)]
+
+        # ans = float("inf")
+        # for j in range(col):
+        #     dp = dict()
+        #     ans = min(ans,f(0,j))
+        # return ans 
+        
 
         
