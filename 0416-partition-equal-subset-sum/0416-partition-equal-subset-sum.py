875a0b17f62ed776d8c62483f9b4888f7fc5d054
Nithish <130085124+NithishCodes-2411@users.noreply.github.com>
2025-01-12 19:34:23 -0500

    Time: 5500 ms (5.64%), Space: 305 MB (5.05%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def canPartition(self, nums):
+        ts = sum(nums)
+        n = len(nums)
+        if ts%2==1:
+            return False
+        
+        memo = {}
+        
+
+        def f(s,p):
+
+            if p == n:
+                return False
+            
+            if s == ts-s:
+                return True
+            if (s,p)  in memo:
+                return memo[(s,p)]
+
+            not_pick = f(s,p+1)
+            if not not_pick :
+                pick = f(s+nums[p] , p+1)
+                memo[(s,p)]= pick 
+                return memo[(s,p)]
+            memo[(s,p)] = not_pick
+            return memo[(s,p)]
+
+
+        return f(0,0)
+        
