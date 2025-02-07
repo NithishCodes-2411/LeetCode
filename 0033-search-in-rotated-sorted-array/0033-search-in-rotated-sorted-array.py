919b684372167549ca9ff32be8c8869e672e0505
Nithish <130085124+NithishCodes-2411@users.noreply.github.com>
2025-02-06 20:31:09 -0500

    Time: 3 ms (4.58%), Space: 18.2 MB (36.51%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,18 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
 
-        n = len(nums)-1
+        n = len(nums)
 
         low , high = 0 , n-1
 
         while low<=high:
 
             mid = (low+high)//2
+            print(mid)
 
             if nums[mid] == target:
                 return mid
             # left half is sorted
-            if nums[low] < nums[mid]:
+            if nums[low] <= nums[mid]:
                 if nums[low] <= target and target <= nums[mid]:
                     high = mid -1
                 else:
@@ -21,7 +22,7 @@ class Solution:
             #right half is sorted
             else:
                 if nums[mid] <= target and target <= nums[high]:
-                    low + mid+1
+                    low = mid+1
                 else:
                     high = mid -1
         return -1
